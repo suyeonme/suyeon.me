a8fc8f80b9e5a258a4e920e6f6c6242f9726eec8
Suyeon Kang <tndusrkd92@gmail.com>
2021-02-03 12:11:42 +0900

    Test displaying loader when language is changed

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 import { I18nextProvider } from 'react-i18next';
-import i18n from '../../i18n';
+import i18n, { setCallback } from '../../i18n';
 
 import 'fonts/fonts.css';
 import SEO from 'components/Layout/SEO/SEO';
@@ -116,6 +116,13 @@ type LayoutProps = {
 };
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const [loading, setLoading] = useState(true);
+
+  if (loading) {
+    setCallback(() => setLoading(false));
+    return <p>Loading...</p>;
+  }
+
   return (
     <I18nextProvider i18n={i18n}>
       <div style={{ overflow: 'hidden' }}>
@@ -132,3 +139,138 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
 };
 
 export default Layout;
+
+// import React from 'react';
+// import { createGlobalStyle } from 'styled-components';
+// import reset from 'styled-reset';
+// import { I18nextProvider } from 'react-i18next';
+// import i18n from '../../i18n';
+
+// import 'fonts/fonts.css';
+// import SEO from 'components/Layout/SEO/SEO';
+// import Nav from 'components/Layout/Nav/Nav';
+// import Footer from 'components/Layout/Footer/Footer';
+
+// const GlobalStyle = createGlobalStyle`
+//   ${reset}
+
+//   *,
+//   *::before,
+//   *::after {
+//       margin: 0;
+//       padding: 0;
+//       box-sizing:border-box;
+//   }
+
+//   html {
+//     ${'' /* 10px = 1rem */}
+//     font-size: 62.5%;
+
+//     @media (max-width: 992px) {
+//       ${'' /* 9px = 1rem */}
+//       font-size: 56.25%;
+//     }
+
+//     @media (max-width: 768px) {
+//       ${'' /* 8px = 1rem */}
+//       font-size: 50%;
+//     }
+
+//     @media (max-width: 320px) {
+//       ${'' /* 7px = 1rem */}
+//       font-size: 43.75%;
+//     }
+
+//     scroll-behavior: smooth;
+//   }
+
+//   body {
+//     font-family: 'Poppins', 'Spoqa Han Sans';
+//     font-weight: 300;
+//     color: black;
+//     background-color: white;
+//     overflow-x: hidden;
+//   }
+
+//   img, video {
+//     width: 100%;
+//     height: 100%;
+//   }
+
+//   button {
+//     color: inherit;
+//     font-family: inherit;
+//     font-size: inherit;
+//     outline: none;
+//     cursor: pointer;
+//     border: none;
+//     background-color: transparent;
+//   }
+
+//   p, ul {
+//     opacity: 0;
+//     transform: translateY(60px);
+//   }
+
+//   p, a, li {
+//     font-size: 1.7rem;
+//   }
+
+//   p {
+//     line-height: 2;
+//   }
+
+//   a {
+//     text-decoration:none;
+//     color:inherit;
+//   }
+
+//   ul, li {
+//     list-style: disc;
+//     list-style-position: inside;
+//   }
+
+//   span {
+//     font-weight: 500;
+//   }
+
+//   ::selection {
+//     background-color: #556052;
+//     color: #fff;
+//   }
+
+//   ::-webkit-scrollbar {
+//   width: 5px;
+//   }
+
+//   ::-webkit-scrollbar-track {
+//     background: #F8F8F8;
+//   }
+
+//   ::-webkit-scrollbar-thumb {
+//     background:  black;
+//     border-radius: 20px;
+//   }
+// `;
+
+// type LayoutProps = {
+//   children: React.ReactNode;
+// };
+
+// const Layout: React.FC<LayoutProps> = ({ children }) => {
+//   return (
+//     <I18nextProvider i18n={i18n}>
+//       <div style={{ overflow: 'hidden' }}>
+//         <SEO />
+//         <GlobalStyle />
+//         <header>
+//           <Nav />
+//         </header>
+//         <main>{children}</main>
+//         <Footer />
+//       </div>
+//     </I18nextProvider>
+//   );
+// };
+
+// export default Layout;
--- src/i18n.tsx
+++ src/i18n.tsx
@@ -5,6 +5,9 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 
 const languages = ['en', 'ko'];
 
+let ready: boolean;
+let readyCallback: () => void;
+
 i18n
   .use(Backend)
   .use(LanguageDetector)
@@ -19,6 +22,44 @@ i18n
     react: {
       useSuspense: false,
     },
+  })
+  .then(() => {
+    ready = true;
+    if (readyCallback) {
+      readyCallback();
+    }
   });
 
+export const setCallback = (callback: () => void): void => {
+  readyCallback = callback;
+  if (ready) {
+    readyCallback();
+  }
+};
+
 export default i18n;
+
+// import i18n from 'i18next';
+// import { initReactI18next } from 'react-i18next';
+// import Backend from 'i18next-http-backend';
+// import LanguageDetector from 'i18next-browser-languagedetector';
+
+// const languages = ['en', 'ko'];
+
+// i18n
+//   .use(Backend)
+//   .use(LanguageDetector)
+//   .use(initReactI18next)
+//   .init({
+//     fallbackLng: 'en',
+//     debug: false,
+//     whitelist: languages,
+//     interpolation: {
+//       escapeValue: false,
+//     },
+//     react: {
+//       useSuspense: false,
+//     },
+//   });
+
+// export default i18n;
